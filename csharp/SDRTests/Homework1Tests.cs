27a5ff8057c74f37c0962aea30202183dadd86a7
Germano Gabbianelli <tyrion.mx@gmail.com>
2013-04-28 23:43:47 +0200

    Aggiunto un piccolo test per la Decimazione (C#)

---
+++ csharp/SDRTests/Homework1Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using SDR;
+
+namespace SDRTests
+{
+    [TestClass]
+    public class Homework1Tests
+    {
+        [TestMethod]
+        public void TestDecimazione()
+        {
+            var seq = Enumerable.Range(0, 50).Select(x => (double)x);
+
+            CollectionAssert.AreEqual(new double[] { 0.0, 25.0 },
+                Homework1.Decimazione(seq, 25).ToArray());
+
+            CollectionAssert.AreEqual(new double[] { 0.0, 10.0, 20.0, 30.0, 40.0},
+                Homework1.Decimazione(seq, 10).ToArray());
+
+        }
+    }
+}
